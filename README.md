41f4071e6cbf6835290d0efb4ddb2ccdbad8d486
shinkeonkim <singun11@kookmin.ac.kr>
2022-04-23 17:28:58 +0900

    feat: canvas를 통해 선 그리기

---
+++ README.md
@@ -0,0 +1,5 @@
+# Drawing
+
+#### 참고
+- [Elcetron Hot Reload](https://jeongsu.tistory.com/245)
+- [JS Canvas 그림판](https://velog.io/@hyunjoogo/JS-%EA%B7%B8%EB%A6%BC%ED%8C%90-%EB%A7%8C%EB%93%A4%EA%B8%B0)
--- renderer.js
+++ renderer.js
@@ -4,6 +4,11 @@ class Renderer {
     this.ctx = this.canvas.getContext('2d');
     this.ctx.translate(0.5, 0.5);
     
+    this.drawing = false;
+    this.lineWidth = 30;
+    this.strokeStyle = 'black';
+    this.lineCap = 'round';
+    
     this.addBindings();
     this.addListeners();
     this.updateCanvasSize();
@@ -11,17 +16,49 @@ class Renderer {
   
   addBindings() {
     this.updateCanvasSize = this.updateCanvasSize.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+    this.draw = this.draw.bind(this);
   }
 
   addListeners() {
     window.addEventListener('resize', this.updateCanvasSize);
+
+
+    this.canvas.addEventListener("mousedown", this.handleMouseDown); // 드로잉 끝
+    this.canvas.addEventListener("mouseup", this.handleMouseUp); // 드로잉 대기
+    this.canvas.addEventListener("mousemove", this.draw);
   }
 
   updateCanvasSize() {
     const width = 80 * window.innerWidth / 100;
     const height = 100 * window.innerHeight / 100;
-    this.canvas.width = this.canvas.style.width = width;
-    this.canvas.height = this.canvas.style.height = height;
+    this.canvas.width = width;
+    this.canvas.height = height;
+  }
+
+  handleMouseDown () {
+    this.drawing = true;
+    this.ctx.lineWidth = this.lineWidth;
+    this.ctx.lineCap = this.lineCap;
+    this.ctx.strokeStyle = this.strokeStyle;
+  }
+
+  handleMouseUp () {
+    this.drawing = false;
+  }
+
+  draw(e) {
+    const x = e.offsetX;
+    const y = e.offsetY;
+
+    if (!this.drawing) return;
+
+
+    this.ctx.moveTo(x, y);
+    this.ctx.beginPath();
+    this.ctx.lineTo(x, y);
+    this.ctx.stroke();  
   }
 }
 
